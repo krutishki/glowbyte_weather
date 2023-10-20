39252a04641e7c9b2c186e713b51c14be1d418a0
Kirill G <gerakir@yandex.ru>
2023-10-20 23:36:21 +0300

    Функция для чтения датасетов

--- datasets.py
+++ datasets.py
@@ -1,5 +1,26 @@
 import pandas as pd
 
+def read_datasets():
+    '''
+    Читает датасеты, используемые в процессе обучения.
+
+    Returns
+    --------
+    Словарь со следующими ключами:
+        * source_train
+        * source_test
+        * weather_parsed
+        * holidays
+        * population
+    '''
+    return {
+        'source_train': pd.read_csv('./data/train_dataset.csv'),
+        'source_test': pd.read_csv('./data/test_dataset.csv'),
+        'weather_parsed': pd.read_csv('./data/weather.csv', delimiter=';', encoding="UTF-8", skiprows=6),
+        'holidays': pd.read_csv('./data/holidays.csv'),
+        'population': pd.read_csv('./data/population.csv'),
+    }
+
 def prepare_dataset(df):
     """
     Вся подготовка датасета и feature engineering
