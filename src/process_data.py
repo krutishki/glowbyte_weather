01aee580aba5f3b8ac48bb852e96ec18ae9c197f
vostenzuk <nyafish@gmail.com>
2023-10-21 15:21:26 +0300

    Add weather processing

--- src/process_data.py
+++ src/process_data.py
@@ -39,3 +39,66 @@ def prepare_dataset(df: pd.DataFrame) -> pd.DataFrame:
     result["ds"] = result["datetime"]
     result["y"] = result["target"]
     return result
+
+
+def process_weather(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Обработка признаков с погодой
+
+    Аргументы:
+        df (pd.DataFrame): Датасет
+    """
+
+    result = df.copy()
+    result["weather_fact"] = result["weather_fact"].fillna("")
+    result["weather_pred"] = result["weather_pred"].fillna("")
+
+    result = process_precipitation(result, "pred")
+    result = process_precipitation(result, "fact")
+    result = process_cloudiness(result, "pred")
+    result = process_cloudiness(result, "fact")
+
+    return result
+
+
+def process_precipitation(df: pd.DataFrame, suffix: str = "") -> pd.DataFrame:
+    result = df.copy()
+
+    weather_col = f"weather_{suffix}"
+    result[f"precipitation_{suffix}"] = "Без осадков"
+    result.loc[result[weather_col].str.contains("дожд"), f"precipitation_{suffix}"] = "Дождь"
+    result.loc[result[weather_col].str.contains("дожь"), f"precipitation_{suffix}"] = "Дождь"
+    result.loc[result[weather_col].str.contains("лив"), f"precipitation_{suffix}"] = "Ливень"
+    result.loc[result[weather_col].str.contains("гроза"), f"precipitation_{suffix}"] = "Ливень"
+    result.loc[result[weather_col].str.contains("шторм"), f"precipitation_{suffix}"] = "Ливень"
+    result.loc[result[weather_col].str.contains("град"), f"precipitation_{suffix}"] = "Град"
+    result.loc[result[weather_col].str.contains("морос"), f"precipitation_{suffix}"] = "Морось"
+    result.loc[result[weather_col].str.contains("снег"), f"precipitation_{suffix}"] = "Снег"
+    result.loc[result[weather_col].str.contains("снеж"), f"precipitation_{suffix}"] = "Снег"
+
+    return result
+
+
+def process_cloudiness(df: pd.DataFrame, suffix: str = "") -> pd.DataFrame:
+    result = df.copy()
+
+    weather_col = f"weather_{suffix}"
+    cloud_col = f"cloudiness_{suffix}"
+
+    result.loc[result[weather_col].str.contains("ясн"), cloud_col] = "Ясно"
+    result.loc[result[weather_col].str.contains("обл"), cloud_col] = "Облачно"
+    result.loc[result[weather_col].str.contains("проясн"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("обл с пр"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("обл. с  пр."), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("обл.с пр"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("облачно с пр"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("обл сп"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("обл. с пр"), cloud_col] = "Прояснения"
+    result.loc[result[weather_col].str.contains("п/обл"), cloud_col] = "Переменная обл"
+    result.loc[result[weather_col].str.contains("пер.обл"), cloud_col] = "Переменная обл"
+    result.loc[result[weather_col].str.contains("пер. обл"), cloud_col] = "Переменная обл"
+    result.loc[result[weather_col].str.contains("перем.обл"), cloud_col] = "Переменная обл"
+    result.loc[result[weather_col].str.contains("малообл"), cloud_col] = "Малооблачно"
+    result.loc[result[weather_col].str.contains("малобл"), cloud_col] = "Малооблачно"
+    result.loc[result[weather_col].str.contains("пасм"), cloud_col] = "Пасмурно"
+    return result
